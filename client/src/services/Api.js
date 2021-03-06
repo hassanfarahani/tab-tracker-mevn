e1bb2fd1b7dd995e896b0f1817cfa9c0af01b1df
Hassan Farahani <farahani.h1360@gmail.com>
2021-03-06 11:43:33 -0800

    app ready to be deployed

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,7 +3,8 @@ import store from '@/store'
 
 export default () => {
     return axios.create({
-        baseURL: `http://localhost:8081`,
+        // baseURL: `http://localhost:8081`,
+        baseURL: `https://tab-tracker-mevn.herokuapp.com/`,
         headers: {
             Authorization: `Bearer ${store.state.token}`
         }
